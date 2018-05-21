c70440b042bcc44b9353f3fd39ba515561c47f9f
simongaviria1 <simongaviria@ac.c4q.nyc>
2018-05-21 14:22:00 -0400

    added CSS styling to the webapp. Made note taking more dynamic and refractored Home.js file (removed unessesary code)

--- frontend/src/ToDo/Home.js
+++ frontend/src/ToDo/Home.js
@@ -1,19 +1,8 @@
 import React, {Component} from 'react'
 
+import Style from '../CSS/style.css'
+
 class Home extends Component {
-    constructor() {
-        super()
-        this.state = {
-            textField: false
-        }
-    }
-
-    handleClick = () => {
-        this.setState({
-            textField: !this.state.textField
-        })
-    }
-
     render() {
         const {
             onSubmitToDoList,
@@ -26,28 +15,33 @@ class Home extends Component {
             toggleComplete,
             toggleMode,
             handleTextField,
+            noteArray,
             mode,
+            note,
             task,
             title,
-            handleKeyPress, 
-            toDoArray
+            handleKeyPress,
+            handleNoteChange,
+            toDoArray,
+            handleClick,
+            textField
         } = this.props
 
-        const {textField} = this.state
-        console.log('toDoArray', toDoArray)
+        console.log('noteArray', noteArray)
         return (
-            <div>
-                <div>
+            <div id='container'>
+                <div id='inputTitle'>
                     {/* renders  title for list */}
-                        <input
-                            placeholder='Title'
-                            type='text'
-                            onChange={handleTitileInput}
-                            name='title'
-                            value={title}
-                            onClick={this.handleClick}/>
+                    <input
+                        id='titleInputBox'
+                        placeholder='Title'
+                        type='text'
+                        onChange={handleTitileInput}
+                        name={mode}
+                        value={title}
+                        onClick={handleClick}/>
 
-                    <button onClick={toggleMode} name='list' className='mode'>
+                    <button className='modeButton' onClick={toggleMode} name='list' className='mode'>
                         List
                     </button>
                     <button onClick={toggleMode} name='note' className='mode'>
@@ -55,10 +49,27 @@ class Home extends Component {
                     </button>
 
                     {/* either renders input box for to do list or text input field for notes */}
-                    {mode === 'list' && textField ? <form onSubmit={onSubmitToDoList}><input name='task' placeholder ='item' type='text' value={task} onKeyPress={handleKeyPress} onChange={handleInput}/></form> : mode === 'note' && textField ? <form onSubmit={onSubmitNotes}><textarea rows="4" cols="50" placeholder='Make a note'></textarea></form> : '' }
-                    
-                </div>
-                <div>
+                    {mode === 'list' && textField
+                        ? <form onSubmit={onSubmitToDoList}><input
+                        id='todoItem'
+                                name='task'
+                                placeholder='item'
+                                type='text'
+                                value={task}
+                                onKeyPress={handleKeyPress}
+                                onChange={handleInput}/></form>
+                        : mode === 'note' && textField
+                            ? <form onSubmit={onSubmitNotes}>
+                                    <textarea
+                                    id='noteTextArea'
+                                        onChange={handleNoteChange}
+                                        value={note}
+                                        rows="1"
+                                        cols="70"
+                                        placeholder='Make a note'></textarea>
+                                </form>
+                            : ''}
+
                     <div>
                         {/* iterates through toDoList and renders list on screen */}
                         <ul>
@@ -68,17 +79,33 @@ class Home extends Component {
                         </ul>
                         <button onClick={submitList}>Done</button>
                     </div>
-                    <div>
+                </div>
+                <div>
+                    <div id='itemContainter'>
                         {/* iterates through toDoList and renders each todo list in its own container  */}
-                        {toDoArray.map(element => { 
-                            return ( 
-                                <div className='toDoBlockContainer'> 
+                        {toDoArray.map(element => {
+                            return (
+                                <div className='toDoBlockContainer'>
                                     <h3 id='title'>{element.title}</h3>
-                                    <ul className='toDoItem'>{element.toDoList.map(ele => { 
-                                            return <li onClick={toggleComplete}>{ele}</li>
-                                        })}
+                                    <ul className='toDoItem'>{element
+                                            .toDoList
+                                            .map(ele => {
+                                                return <li onClick={toggleComplete}>{ele}</li>
+                                            })}
                                     </ul>
-                           </div>)
+                                </div>
+                            )
+                        })}
+
+                        {/* iterates through noteArray and renders each todo list in its own container  */}
+                        {noteArray.map(ele => {
+                            console.log('title', ele.title)
+                            return (
+                                <div className='noteBlock'>
+                                    <h3>{ele.title}</h3>
+                                    <p>{ele.note}</p>
+                                </div>
+                            )
                         })}
                     </div>
                 </div>
--- frontend/src/ToDo/ToDoRouter.js
+++ frontend/src/ToDo/ToDoRouter.js
@@ -4,108 +4,138 @@ import {Link, Switch, Route} from 'react-router-dom'
 //inport compnents
 import Home from './Home'
 
+import Style from '../CSS/style.css'
+
 class ToDoRouter extends Component {
     constructor() {
         super();
         this.state = {
             mode: 'list', //Default mode
+            textField: false,
             note: '',
             list: '',
-            title:'',
+            title: '',
             task: '',
-            toDoList: [], 
+            toDoList: [],
+            noteArray: [],
             toDoArray: []
         }
     }
 
-    // Handles todo list submittion 
+    // Handles todo list submittion
     submitList = e => {
         e.preventDefault();
-        const {toDoArray, toDoList, task, title} = this.state
-        this.setState({
-            toDoArray: [
-                ...toDoArray, {
-                    title: title,
-                    toDoList: toDoList,
-                    complete: false
-                }
-            ],
-            title: '', 
-            task: '', 
-            toDoList: []
-        }) 
-    }
-
-    //Handles notes submission 
-    onSubmitNotes = e => {
-        e.preventDefault();
-        console.log('handle note submission')
-    }
-
-    
-    toggleComplete = e => {
-        console.log('toggleComplete', 'clicked')
-    }
-    
-    //Sets mode to list or note 
-    toggleMode = e => {
-        this.setState({mode: e.target.name})
-    }
-    
-    handleKeyPress = e => { 
-        if (e.key === 'Enter'){ 
-            this.setState({ 
-                task : ''
+        const {
+            toDoArray,
+            toDoList,
+            task,
+            title,
+            note,
+            noteArray
+        } = this.state
+        // ternary to submit either todo list or note
+        this.state.mode === 'list'
+            ? this.setState({
+                toDoArray: [
+                    ...toDoArray, {
+                        title: title,
+                        toDoList: toDoList,
+                        complete: false
+                    }
+                ],
+                title: '',
+                task: '',
+                toDoList: [],
+                textField: false
             })
-        }
+            : this.setState({
+                noteArray: [
+                    ...noteArray, {
+                        title: title,
+                        note: note
+                    }
+                ],
+                note: '',
+                title: '',
+                textField: false
+
+            })
     }
-    
-    //Handles user input for text input box
+
+    // Change this name
+    onSubmitToDoList = e => {
+        e.preventDefault();
+        const {toDoList, task} = this.state
+        this.setState({
+            toDoList: [
+                ...toDoList,
+                task
+            ],
+            task: ''
+        })
+    }
+
+    handleClick = () => {
+        this.setState({textField: true})
+    }
+
+    //Sets mode to list or note
+    toggleMode = e => {
+        this.setState({mode: e.target.name, textField: true})
+    }
+
     handleInput = e => {
         const {task} = this.state
         console.log(e.target.value)
-        this.setState({ 
-            task: e.target.value
-        })
-        
+        this.setState({task: e.target.value})
+
     }
-    
-    //Handles input for title input box 
-    handleTitileInput = e => { 
-        this.setState({ 
-            title: e.target.value
-        })
+
+    handleNoteChange = e => {
+        this.setState({note: e.target.value})
     }
-    
-    //Handles user input for text field 
+
+    //Handles input for title input box
+    handleTitileInput = e => {
+        this.setState({title: e.target.value})
+    }
+
+    //Handles user input for text field
     handleTextField = e => {
         console.log('text input', e.target.name)
     }
 
-    onSubmitToDoList = e => { 
-        e.preventDefault();
-        const {toDoList,task} = this.state
-        this.setState({ 
-            toDoList: [...toDoList, task],
-            task: '' 
-        }) 
-    }
-    
     renderTodoList = () => {
-        const {input, toDoList, mode, task, title, toDoArray} = this.state
+        const {
+            input,
+            toDoList,
+            mode,
+            task,
+            title,
+            toDoArray,
+            noteArray,
+            note,
+            textField
+        } = this.state
+
         return <Home
             onSubmitToDoList={this.onSubmitToDoList}
             submitList={this.submitList}
             handleTitileInput={this.handleTitileInput}
             handleInput={this.handleInput}
+            handleClick={this.handleClick}
+            handleNoteChange={this.handleNoteChange}
             onSubmitNotes={this.onSubmitNotes}
             toDoList={toDoList}
             toDoArray={toDoArray}
+            noteArray={noteArray}
             toggleComplete={this.toggleComplete}
             toggleMode={this.toggleMode}
             handleTextField={this.handleInput}
             task={task}
+            note={note}
             title={title}
+            textField={textField}
             mode={mode}/>
     }
 
@@ -113,7 +143,7 @@ class ToDoRouter extends Component {
         return (
             <div>
                 <div>
-                    <nav>
+                    <nav id='nav'>
                         <Link to='/'>Home</Link>
                     </nav>
                 </div>
