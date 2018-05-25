1da343be1ae8b4fa04f4726b048dab9c0f0c3034
simongaviria1 <simongaviria@ac.c4q.nyc>
2018-05-25 13:36:36 -0400

    readded node_modules folder due to proxy issues

--- backend/auth/passport.js
+++ backend/auth/passport.js
@@ -1,42 +1,22 @@
-const bcrypt = require("bcryptjs");
+const passport = require("passport");
 const db = require("../db/index");
 
-function comparePass(userPassword, databasePassword) {
-    return bcrypt.compareSync(userPassword, databasePassword);
-}
-
-function createHash(password) {
-    const salt = bcrypt.genSaltSync();
-    const hash = bcrypt.hashSync(password, salt);
-    return hash;
-}
-
-function createUser(req) {
-    const salt = bcrypt.genSaltSync();
-    const hash = bcrypt.hashSync(req.body.password, salt);
-    return db.none("INSERT INTO users (username, password_digest, email, fullName) VALUES (${usernam" +
-            "e}, ${password}, ${email}, ${fullName})", {
-        username: req.body.username,
-        password: hash,
-        email: req.body.email,
-        fullName: req.body.fullName
+module.exports = () => {
+    // store the user id on the session
+    passport.serializeUser((user, done) => {
+        done(null, user);
     });
-}
 
-function loginRequired(req, res, next) {
-
-    if (!req.user) {
-        res
-            .status(401)
-            .json({status: "Please log in."});
-        return;
-    }
-    next();
-}
-
-module.exports = {
-    comparePass,
-    createHash,
-    loginRequired,
-    createUser
+    // get user info from session
+    passport.deserializeUser((user, done) => {
+        console.log("deserializing", user);
+        db
+            .one("SELECT * FROM users WHERE id=$1", [user.id])
+            .then(user => {
+                done(null, user);
+            })
+            .catch(err => {
+                done(err, null);
+            });
+    });
 };
