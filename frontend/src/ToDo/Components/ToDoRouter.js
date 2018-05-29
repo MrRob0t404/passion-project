d2802ccf2dbc8cab8b58cd2637c62f5d879d42e4
simongaviria1 <simongaviria@ac.c4q.nyc>
2018-05-29 16:37:48 -0400

    moved ToDoRouter.js file into Components file

--- frontend/src/ToDo/Components/ToDoRouter.js
+++ frontend/src/ToDo/Components/ToDoRouter.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react'
-import {Link, Switch, Route} from 'react-router-dom'
-// import LoginUser from '.././Login/login'
-
-//inport compnents
+import {Link, Switch, Route, Redirect} from 'react-router-dom'
+import axios from 'axios'
+// import LoginUser from '.././Login/login' inport compnents
 import Home from './Home'
 
 import Style from '../.././CSS/style.css'
@@ -19,8 +18,18 @@ class ToDoRouter extends Component {
             task: '',
             toDoList: [],
             noteArray: [],
-            toDoArray: []
+            toDoArray: [],
+            user: null
         }
+    }
+
+    componentWillMount = () => {
+        const {noteArray} = this.state
+        this.setState({user: this.props.user})
+        axios('/users/getNotes').then(res => {
+            // console.log('axios data', res)
+            this.setState({noteArray: res.data.user})
+        })
     }
 
     // Handles todo list submittion
@@ -32,8 +41,24 @@ class ToDoRouter extends Component {
             task,
             title,
             note,
+            mode,
             noteArray
         } = this.state
+
+        // Performs axios request to the backend (handles both requests for notes and
+        // list items)
+        if (mode === 'note') {
+            axios.post('/users/postNote', {
+                title: title,
+                note: note,
+                user_id: this.props.user.id
+            })
+        } else {
+            axios.post('users/postToDoList', {
+                title: title,
+                toDoList: toDoList
+            })
+        }
 
         // ternary to submit either todo list or note
         this.state.mode === 'list'
@@ -60,11 +85,10 @@ class ToDoRouter extends Component {
                 note: '',
                 title: '',
                 textField: false
-
             })
     }
 
-    // Change this name
+    // Change this name: Adds new todo item in the todo array
     onSubmitToDoListForPreview = e => {
         e.preventDefault();
         const {toDoList, task} = this.state
@@ -87,7 +111,7 @@ class ToDoRouter extends Component {
         this.setState({mode: e.target.name, textField: true})
     }
 
-    //handles secondary input 
+    //handles secondary input
     handleInput = e => {
         const {task} = this.state
         // console.log(e.target.value)
@@ -95,9 +119,9 @@ class ToDoRouter extends Component {
 
     }
 
-    //Not sure what this does yet 
+    //Not sure what this does yet
     toggleComplete = e => {
-        console.log('clicked', e.target.value)
+        // console.log('clicked', e.target.value)
     }
 
     //Handles note changing
@@ -112,11 +136,11 @@ class ToDoRouter extends Component {
 
     //Handles user input for text field
     handleTextField = e => {
-        console.log('text input', e.target.name)
+        // console.log('text input', e.target.name)
     }
 
     removeBlock = () => {
-        console.log('removed')
+        // console.log('removed')
     }
 
     // Renders todo list
@@ -130,52 +154,54 @@ class ToDoRouter extends Component {
             toDoArray,
             noteArray,
             note,
-            textField
+            textField,
+            user
         } = this.state
-
-        return <Home
-            onSubmitToDoListForPreview={this.onSubmitToDoListForPreview}
-            submitList={this.submitList}
-            handleTitileInput={this.handleTitileInput}
-            handleInput={this.handleInput}
-            handleClick={this.handleClick}
-            handleNoteChange={this.handleNoteChange}
-            toDoList={toDoList}
-            toDoArray={toDoArray}
-            noteArray={noteArray}
-            toggleComplete={this.toggleComplete}
-            toggleMode={this.toggleMode}
-            handleTextField={this.handleInput}
-            removeBlock={this.removeBlock}
-            task={task}
-            note={note}
-            title={title}
-            textField={textField}
-            mode={mode}/>
+        if (user) {
+            return <Home
+                onSubmitToDoListForPreview={this.onSubmitToDoListForPreview}
+                submitList={this.submitList}
+                handleTitileInput={this.handleTitileInput}
+                handleInput={this.handleInput}
+                handleClick={this.handleClick}
+                handleNoteChange={this.handleNoteChange}
+                toDoList={toDoList}
+                toDoArray={toDoArray}
+                noteArray={noteArray}
+                toggleComplete={this.toggleComplete}
+                toggleMode={this.toggleMode}
+                handleTextField={this.handleInput}
+                removeBlock={this.removeBlock}
+                task={task}
+                note={note}
+                title={title}
+                textField={textField}
+                mode={mode}
+                user={user}/>
+        } else {
+            return < Redirect to = '/login' />
     }
+}
 
-    // renderLogin = () => {
-    //     return <LoginUser/>
-    // }
-
-    render() {
-        return (
+render() {
+    console.log('notes', this.state.noteArray)
+    return (
+        <div>
             <div>
-                <div>
-                    <nav id='nav'>
-                        <Link to='/'>Home</Link>{' '}
-                        <Link to='/logout'>Logout</Link>{' '}
-                    </nav>
-                </div>
-                <div>
-                    <Switch>
-                        {/* <Route path='/login' component={this.renderLogin}/> */}
-                        <Route exact path='/' component={this.renderTodoList}/>
-                    </Switch>
-                </div>
+                <nav id='nav'>
+                    <button onClick={this.props.logOut}>Logout</button>{' '}
+                    {/* <h2>Hello, {this.state.user.username}</h2> */}
+                </nav>
             </div>
-        )
-    }
+            <div>
+                <Switch>
+                    {/* <Route path='/login' component={this.renderLogin}/> */}
+                    <Route exact path='/' component={this.renderTodoList}/>
+                </Switch>
+            </div>
+        </div>
+    )
+}
 }
 
 export default ToDoRouter
