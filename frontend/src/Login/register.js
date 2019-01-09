7a38ec060236c88e92a8fac402d931a37947d0e6
Simon <simongaviria@pursuit.org>
2019-01-08 22:45:50 -0500

    removed redundant blocks and fixed warning messages when compiled

--- frontend/src/Login/register.js
+++ frontend/src/Login/register.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import Style from '.././CSS/style.css'
+import '.././CSS/style.css'
 
 class RegisterUser extends React.Component {
     constructor() {
@@ -28,38 +28,35 @@ class RegisterUser extends React.Component {
     // made
     handleRegisterButton = e => {
         e.preventDefault()
-        const {fullName, username, password, verifyPassword, email} = this.state;
-        if (!fullName || !username, !password || !verifyPassword, !email) {
-            this.setState({message: 'Please fill out all fields.'})
+        const { fullName, username, password, verifyPassword, email } = this.state;
+        if (!fullName || !username || !password || !verifyPassword || !email) {
+            this.setState({ message: 'Please fill out all fields.' })
         } else if (username.length < 4 || password.length < 4) {
-            this.setState({message: 'Username and Password must be at least 4 characters.'})
+            this.setState({ message: 'Username and Password must be at least 4 characters.' })
         } else if (password !== verifyPassword) {
-            this.setState({message: 'Passwords do not match.'})
+            this.setState({ message: 'Passwords do not match.' })
         } else {
             axios
                 .post('/users/new', {
-                username: username,
-                password: password,
-                email: email,
-                fullName: fullName
-            })
+                    username: username,
+                    password: password,
+                    email: email,
+                    fullName: fullName
+                })
                 .then(() => {
                     axios.post("/users/login", {
                         username: username,
                         password: password
                     }).then(res => {
-                        console.log(`res`, res)
                         this
                             .props
                             .setUser(res.data.user)
                     }).catch(err => {
-                        console.log(err)
-                        this.setState({message: 'Error logging in'})
+                        this.setState({ message: 'Error logging in' })
                     })
 
                 })
                 .catch(err => {
-                    console.log(`this is your`, err)
                     this.setState({
                         email: '',
                         password: '',
@@ -76,17 +73,6 @@ class RegisterUser extends React.Component {
     }
 
     render() {
-        const {
-            message,
-            fullName,
-            email,
-            password,
-            verifyPassword,
-            username,
-            loggedIn
-        } = this.state
-
-        console.log(this.state)
         return (
             <div className="limiter">
                 <div className="container-login100">
@@ -106,7 +92,7 @@ class RegisterUser extends React.Component {
                                     type="text"
                                     name="fullName"
                                     placeholder="Name"
-                                    onChange={this.handleInput}/>
+                                    onChange={this.handleInput} />
                                 <span className="focus-input100" data-placeholder="&#xf207;"></span>
                             </div>
 
@@ -116,7 +102,7 @@ class RegisterUser extends React.Component {
                                     type="email"
                                     name="email"
                                     placeholder="Email"
-                                    onChange={this.handleInput}/>
+                                    onChange={this.handleInput} />
                                 <span className="focus-input100" data-placeholder="&#xf207;"></span>
                             </div>
 
@@ -126,7 +112,7 @@ class RegisterUser extends React.Component {
                                     type="text"
                                     name="username"
                                     placeholder="Username"
-                                    onChange={this.handleInput}/>
+                                    onChange={this.handleInput} />
                                 <span className="focus-input100" data-placeholder="&#xf207;"></span>
                             </div>
 
@@ -136,7 +122,7 @@ class RegisterUser extends React.Component {
                                     type="password"
                                     name="password"
                                     placeholder="Password"
-                                    onChange={this.handleInput}/>
+                                    onChange={this.handleInput} />
                                 <span className="focus-input100" data-placeholder="&#xf191;"></span>
                             </div>
 
@@ -146,7 +132,7 @@ class RegisterUser extends React.Component {
                                     type="password"
                                     name="verifyPassword"
                                     placeholder="Confirm Password"
-                                    onChange={this.handleInput}/>
+                                    onChange={this.handleInput} />
                                 <span className="focus-input100" data-placeholder="&#xf191;"></span>
                             </div>
 
@@ -175,59 +161,3 @@ class RegisterUser extends React.Component {
 }
 
 export default RegisterUser
-
-{/* <div className="auth" id="register">
-<div className="auth-container" id="register-container">
-    <h2>Note cloud</h2>
-    <form>
-        <input
-            className='loginInput'
-            name="fullName"
-            placeholder="Full Name"
-            type="text"
-            onChange={this.handleInput}
-            value={fullName}/>
-        <input
-            className='loginInput'
-            name="email"
-            type="email"
-            placeholder="Email"
-            onChange={this.handleInput}
-            value={email}/>
-        <input
-            className='loginInput'
-            name="username"
-            type="text"
-            placeholder="Username"
-            onChange={this.handleInput}
-            value={username}/>
-        <input
-            className='loginInput'
-            name="password"
-            type="password"
-            placeholder="Password"
-            onChange={this.handleInput}
-            value={password}/>
-        <input
-            className='loginInput'
-            name="verifyPassword"
-            type="password"
-            placeholder="Re-enter Password"
-            onChange={this.handleInput}
-            value={verifyPassword}/>
-        <input
-            className='loginInput'
-            type="submit"
-            className="auth-button"
-            onClick={this.handleRegisterButton}value="Register"/>
-    </form>
-    <p>{" "}
-        Already a TyroDev member?
-        <Link to='/login'>
-            Login
-        </Link>
-    </p>
-    <p>{message}</p>
-</div>
-</div> */
-}
